b4caa855a6496efd5ef685ba246155bbf3599823
darren.haken <darrenhaken@gmail.com>
2013-11-12 10:44:40 +0000

    Updated wkhtmltopdf as it has been updated for Windows support through an accepted pull request.

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 var http = require('http'),
-    htmlToPdf = require('wkhtmltopdf');
+    htmlToPdf = require('wkhtmltopdf'),
+    path = require('path');
 
 http
     .createServer(acceptHtmlAndProvidePdf)
     .listen(1337, '127.0.0.1');
 
 function acceptHtmlAndProvidePdf(request, response) {
+    console.log('Request received: ' + request);
+
     request.addListener("data", function (chunk) {
         request.content += chunk;
     });
 
     request.addListener("end", function () {
-        response.writeHead(200, {'Content-Type': 'application/pdf'});
-        htmlToPdf(request.content).pipe(response);
+        response.writeHead(200, { 'Content-Type': 'application/pdf' });
+
+        htmlToPdf(request.content)
+            .pipe(response);
+
+        console.log('Processed HTML to PDF: ' + response);
     });
 }
 
 console.log('Server running at http://127.0.0.1:1337/');
+
+
+
 
